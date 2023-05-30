8161a6fb83fcf9939e58fefa062ca2ff0ff6b8f9
Hajin Choi <hchoi405@gmail.com>
2023-05-30 17:01:16 +0900

    stage

--- test_final.py
+++ test_final.py
@@ -4,14 +4,10 @@ import subprocess
 
 num_frames = 301
 
-# Based on SSIM loss and ~103 frames
-nbg_best_models = {
-    "data_Arcade": "san-miguel",
-    "data_BistroExterior2": "sponza-moving-light",
-    "data_Classroom": "sponza-glossy",
-    "data_Dining-room": "sponza-glossy",
-    "data_Staircase2": "san-miguel"
-}
+# Based on PSNR loss and ~103 frames
+
+scenes = ["data_Classroom", "data_Dining-room", "data_Staircase"]
+models = ["classroom"]
 
 def rename_directory(old_name, new_name):
     # Rename the directory
@@ -32,32 +28,34 @@ def rename_directory(old_name, new_name):
             shutil.rmtree(old_name)
 
 # Iterate scenes
-for scene, model in nbg_best_models.items():
-    dst_dir = f"output/{scene}"
-    os.makedirs(dst_dir, exist_ok=True)
+for scene in scenes:
+    for model in models:
+        dst_dir = f"output/{scene}_{model}"
+        os.makedirs(dst_dir, exist_ok=True)
 
-    print(f"Processing scene: {scene}, total frame: {num_frames}, model: {model}")
-    for i in range(num_frames):
-        print(f'\tframe: {i}')
-        
-        # command = f"python network_test.py -m classroom -s test -r ./dataset -d classroom -ts 60 -t 0"
-        # command = f"python network_test.py -m sponza-moving-light -s test_Falcor -r ./dataset_falcor -d data_BistroExterior2 -ts 103 -t 0"
-        command = f"python network_test.py -m {model} -s test_Falcor -r /dataset_falcor/dataset_new -d {scene} -ts {num_frames} -t {i} --export_exr"
-        try:
-            subprocess.check_call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        except subprocess.CalledProcessError as e:
-            print(f"Error: {e.output.decode()}")
+        print(f"Processing scene: {scene}, total frame: {num_frames}, model: {model}")
+        for i in range(num_frames):
+            if scene == "data_Classroom" and i < 180: continue
+            print(f'\tframe: {i}')
+            
+            # command = f"python network_test.py -m classroom -s test -r ./dataset -d classroom -ts 60 -t 0"
+            # command = f"python network_test.py -m sponza-moving-light -s test_Falcor -r ./dataset_falcor -d data_BistroExterior2 -ts 103 -t 0"
+            command = f"python network_test.py -m {model} -s test_Falcor -r /dataset_falcor/dataset_new -d {scene} -ts {num_frames} -t {i} --export_exr"
+            try:
+                subprocess.check_call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            except subprocess.CalledProcessError as e:
+                print(f"Error: {e.output.decode()}")
 
-        for file in os.listdir(f"results/{scene}/result/test_out"):
-            src = f'results/{scene}/result/test_out/{file}'
-            dst = f'{dst_dir}/{file}'
-            os.rename(src, dst)
+            for file in os.listdir(f"results/{scene}/result/test_out"):
+                src = f'results/{scene}/result/test_out/{file}'
+                dst = f'{dst_dir}/{file}'
+                os.rename(src, dst)
 
-    # Make sure the directory exists in the NAS
-    os.makedirs(f"/nas/nbg/{scene}/", exist_ok=True)
+        # # Make sure the directory exists in the NAS
+        # os.makedirs(f"/nas/nbg/{scene}/", exist_ok=True)
 
-    # # Move all files through rsync and remove original files in asynchronous process
-    subprocess.Popen(["rsync", "-av", "--no-o", "--no-g", "--remove-source-files", 
-        dst_dir,
-        f"/nas/nbg/"
-        ])
+        # # # Move all files through rsync and remove original files in asynchronous process
+        # subprocess.Popen(["rsync", "-av", "--no-o", "--no-g", "--remove-source-files", 
+        #     dst_dir,
+        #     f"/nas/nbg/"
+        #     ])
